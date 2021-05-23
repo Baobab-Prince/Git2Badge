b14670bcfe8d51a956b884e3bb2d2c9fd6880aeb
Chaz Carothers <chazcarothers@chazs-mbp.attlocal.net>
2021-05-23 07:48:16 -0500

    Dolly

---
+++ app.js
@@ -0,0 +1,11 @@
+var nickname = 'Moose';
+
+if (nickname == 'Moose') {
+  alert('Hey, Moose!');
+} else if (nickname == 'Bug') {
+  alert("What's Up Bug?");
+} else if (nickname == 'Dolly') {
+  alert('Hello, Dolly!!');
+} else {
+  alert('Who are you?');
+}
